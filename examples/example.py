306c17fab66d326baa19d0d84e9e9b09360813c1
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-11-04 17:01:17 -0800

    Add valid example

--- examples/example.py
+++ examples/example.py
@@ -3,6 +3,7 @@ import os
 
 import aind_behavior_services.calibration.load_cells as lcc
 import aind_behavior_services.rig as rig
+import aind_behavior_telekinesis.task_logic as tl
 from aind_behavior_services import db_utils as db
 from aind_behavior_services.calibration.aind_manipulator import (
     AindManipulatorCalibration,
@@ -20,9 +21,6 @@ from aind_behavior_services.calibration.water_valve import (
 )
 from aind_behavior_services.session import AindBehaviorSessionModel
 from aind_behavior_telekinesis.rig import AindManipulatorDevice, AindTelekinesisRig, RigCalibration
-from aind_behavior_telekinesis.task_logic import (
-    AindTelekinesisTaskLogic,
-)
 
 
 def mock_session() -> AindBehaviorSessionModel:
@@ -103,8 +101,58 @@ def mock_rig() -> AindTelekinesisRig:
     )
 
 
-def mock_task_logic() -> AindTelekinesisTaskLogic:
-    raise NotImplementedError("This function is not implemented")
+def mock_task_logic() -> tl.AindTelekinesisTaskLogic:
+    prototype_trial = tl.Action(
+        reward_probability=tl.scalar_value(1),
+        reward_amount=tl.scalar_value(1),
+        reward_delay=tl.scalar_value(0),
+        action_duration=tl.scalar_value(0.2),
+        is_operant=True,
+        time_to_collect=tl.scalar_value(5),
+        lower_action_threshold=tl.scalar_value(0),
+        upper_action_threshold=tl.scalar_value(20000),
+        continuous_feedback=tl.ManipulatorFeedback(converter_lut_input=[0, 0.2, 1], converter_lut_output=[1, 5, 10]),
+    )
+    return tl.AindTelekinesisTaskLogic(
+        task_parameters=tl.AindTelekinesisTaskParameters(
+            rng_seed=None,
+            environment=tl.Environment(
+                block_statistics=[
+                    tl.BlockGenerator(
+                        block_size=tl.scalar_value(1000),
+                        trial_statistics=tl.Trial(
+                            inter_trial_interval=tl.scalar_value(1),
+                            quiescence_period=tl.QuiescencePeriod(
+                                duration=tl.scalar_value(1), has_cue=True, action_threshold=2000
+                            ),
+                            response_period=tl.ResponsePeriod(
+                                duration=tl.scalar_value(1), has_cue=True, action=prototype_trial
+                            ),
+                            action_source_0=tl.LoadCellActionSource(channel=0),
+                            action_source_1=tl.BehaviorAnalogInputActionSource(channel=0),
+                            lut_reference="amazing_lut",
+                        ),
+                    )
+                ],
+            ),
+            operation_control=tl.OperationControl(
+                spout=tl.SpoutOperationControl(
+                    default_retracted_position=10, default_extended_position=20, enabled=False
+                ),
+                action_luts={
+                    "amazing_lut": tl.ActionLookUpTableFactory(
+                        path="amazing_lut.tiff",
+                        offset=0,
+                        scale=1,
+                        action0_min=0,
+                        action0_max=20000,
+                        action1_min=0,
+                        action1_max=2048,
+                    )
+                },
+            ),
+        )
+    )
 
 
 def mock_subject_database() -> db.SubjectDataBase:
